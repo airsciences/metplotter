15c1c041bb78031026a4078e9274703a08f2879a
ndtipple <nate@airsci.com>
2016-08-17 14:39:49 -0600

    Revert "Revert "Troubleshooting with Adding X-Axis""
    This reverts commit 34adbcfdae269fc42911ae41acadeaf7be0d4515.

--- js/plotting.js
+++ js/plotting.js
@@ -43,12 +43,53 @@
           return console.log(log);
         };
       }
+      this.getDefinition();
     }
 
     LinePlot.prototype.getDefinition = function() {
-      var preError;
+      var _, colorScale, height, margin, preError, width, yDef;
       preError = this.preError + "getDefinition():";
-      return this.log(this.preError + (preError + "JeffIsCool"), "cat", 8);
+      this.log(this.preError + (preError + "JeffIsCool"), "cat", 8);
+      _ = this;
+      width = Math.round($(this.options.target).width());
+      height = Math.round(width / 4);
+      if (this.options.theme === 'minimum') {
+        margin = {
+          top: height * 0.28,
+          right: width * 0.03,
+          bottom: height * 0.18,
+          left: width * 0.03,
+          xOffset: 0
+        };
+      } else {
+        margin = {
+          top: height * 0.07,
+          right: width * 0.03,
+          bottom: height * 0.07,
+          left: width * 0.03,
+          xOffset: 0
+        };
+      }
+      this.log(preError + " (margin):", margin);
+      yDef = d3.scale.log().range([height - margin.bottom - margin.xOffset, margin.top]);
+      if (this.options.theme !== 'minimum') {
+        this.options.x.ticks = d3.time.format(this.options.x.format);
+      }
+      if (this.options.theme === 'airsci') {
+        colorScale = d3.scale;
+      } else {
+        colorScale = d3.scale.category10();
+      }
+      return this.definition = {
+        dimensions: {
+          width: width,
+          height: height,
+          margin: margin,
+          colorScale: colorScale,
+          x: d3.time.scale().range([margin.left, width - margin.right]),
+          y: yDef
+        }
+      };
     };
 
     LinePlot.prototype.responsive = function() {
@@ -72,7 +113,11 @@
       preError = this.preError + "append()";
       _ = this;
       this.log("" + preError, this.options);
-      return this.svg = d3.select(this.options.target).append("svg");
+      this.svg = d3.select(this.options.target).append("svg").attr("class", "line-plot").attr("width", this.definition.dimensions.width).attr("height", this.definition.dimensions.height);
+      this.svg.append("g").attr("class", "line-plot-axis-x").attr("transform", "translate(0, " + (parseFloat(innerHeight)) + ")").style("fill", "none").stroke("stroke", this.options.axisColor).call(this.definition.xAxis);
+      if (this.options.theme !== 'minimum') {
+        return this.svg.select("line-plot-axis-x").selectAll("text").style("font-weight", this.options.font.weight);
+      }
     };
 
     LinePlot.prototype.update = function(data) {};
