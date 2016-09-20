3d697ff22804f4bb13cb9c541e8f405c436a2719
Jacob Fielding <jacob@airsci.com>
2016-09-20 15:36:08 -0600

    Updating to basic latest

--- coffee/line-plot.coffee
+++ coffee/line-plot.coffee
@@ -24,9 +24,20 @@ window.Plotting.LinePlot = class LinePlot
         ticks: 5
         min: null
         max: null
+      yBand:
+        minVariable: null
+        maxVariable: null
+      y2:
+        variable: null
+        ticks: 5
+        min: null
+        max: null
+      y2Band:
+        minVariable: null
+        maxVariable: null
       transitionDuration: 300
-      line1Color: "rgb(41,128,185)"
-      line2Color: "rgb(211, 84, 0)"
+      line1Color: "rgb(41, 128, 185)"
+      line2Color: "rgb(39, 174, 96)"
       weight: 2
       axisColor: "rgb(0,0,0)"
       font:
@@ -36,12 +47,17 @@ window.Plotting.LinePlot = class LinePlot
         weight: 1
         color: "rgb(149, 165, 166)"
       focusCircle:
-        color: "rgb(44, 62, 80)"
+        color: "rgb(41, 128, 185)"
+      focusCircle2:
+        color: "rgb(39, 174, 96)"
 
     if options.x
       options.x = Object.mergeDefaults(options.x, defaults.x)
     if options.y
       options.y = Object.mergeDefaults(options.y, defaults.y)
+    if options.y2
+      options.y2 = Object.mergeDefaults(options.y2, defaults.y2)
+
     @options = Object.mergeDefaults options, defaults
 
     # Wrapped Logging Functions
@@ -58,12 +74,27 @@ window.Plotting.LinePlot = class LinePlot
       @data[key] =
         x: @parseDate(row[@options.x.variable])
         y: row[@options.y.variable]
+      if @options.y2.variable != null
+        @data[key].y2 = row[@options.y2.variable]
+      if (
+        @options.yBand.minVariable != null and
+        @options.yBand.maxVariable != null
+      )
+        @data[key].yMin = row[@options.yBand.minVariable]
+        @data[key].yMax = row[@options.yBand.maxVariable]
+      if (
+        @options.y2Band.minVariable != null and
+        @options.y2Band.maxVariable != null
+      )
+        @data[key].y2Min = row[@options.y2Band.minVariable]
+        @data[key].y2Max = row[@options.y2Band.maxVariable]
+
 
     # Run Get Definition on Class Construction
     @getDefinition()
 
-    # Responsive Event Listener
-    ##################### add recalculation when the window changes ###########
+    # Responsive Event Listener ######## is this the right spot for this
+    ##################### add recalculation when the window changes
     if window.attachEvent
       window.attachEvent 'onresize', ->
         alert 'attachEvent - resize'
@@ -78,8 +109,6 @@ window.Plotting.LinePlot = class LinePlot
   getDefinition: ->
     preError = "#{@preError}getDefinition():"
     _ = @
-
-
 
     # Allow Ticks When Large Screen
     if @options.theme isnt 'minimum'
@@ -97,8 +126,10 @@ window.Plotting.LinePlot = class LinePlot
     # Define D3 Methods
     @definition.xAxis = d3.axisBottom().scale(@definition.x)
       .ticks(Math.round($(@options.target).width() / 100))
+
     @definition.yAxis = d3.axisLeft().scale(@definition.y)
       .ticks(@options.y.ticks)
+
     @definition.line = d3.line()
       .defined((d)->
         !isNaN(d.y) and d.y isnt null
@@ -107,12 +138,39 @@ window.Plotting.LinePlot = class LinePlot
       .y((d) -> _.definition.y(d.y))
       .curve(d3.curveCatmullRom.alpha(0.5))
 
+    @definition.line2 = d3.line()
+      .defined((d)->
+        !isNaN(d.y2) and d.y2 isnt null
+      )
+      .x((d) -> _.definition.x(d.x))
+      .y((d) -> _.definition.y(d.y2))
+      .curve(d3.curveCatmullRom.alpha(0.5))
+
+    @definition.area = d3.area()
+      .defined((d)->
+        !isNaN(d.y) and d.y isnt null
+      )
+      .x((d) -> _.definition.x(d.x))
+      .y0((d) -> _.definition.y(d.yMin))
+      .y1((d) -> _.definition.y(d.yMax))
+      .curve(d3.curveCatmullRom.alpha(0.5))
+
+    @definition.area2 = d3.area()
+      .defined((d)->
+        !isNaN(d.y) and d.y isnt null
+      )
+      .x((d) -> _.definition.x(d.x))
+      .y0((d) -> _.definition.y(d.y2Min))
+      .y1((d) -> _.definition.y(d.y2Max))
+      .curve(d3.curveCatmullRom.alpha(0.5))
+
   calculateChartDims: ->
     preError = "#{@preError}calculateChartDims()"
 
     # Calculate Basic DOM & SVG Dimensions
     width = Math.round($(@options.target).width())
     height = Math.round(width/2.5)
+
     if @options.theme is 'minimum'
       margin =
         top: Math.round(height * 0.28)
@@ -142,10 +200,38 @@ window.Plotting.LinePlot = class LinePlot
     else @parseDate(@options.x.min)
     xmax = if @options.x.max is null then d3.max(data, (d)-> d.x)
     else @parseDate(@options.x.max)
-    ymin = if @options.y.min is null then d3.min(data, (d)-> d.y)
+    y1min = if @options.y.min is null then d3.min(data, (d)-> d.y)
     else @options.y.min
-    ymax = if @options.y.max is null then d3.max(data, (d)-> d.y)
+    y1max = if @options.y.max is null then d3.max(data, (d)-> d.y)
     else @options.y.max
+    y2min = if @options.y2.min is null then d3.min(data, (d)-> d.y2)
+    else @options.y2.min
+    y2max = if @options.y2.max is null then d3.max(data, (d)-> d.y2)
+    else @options.y2.max
+    yBandMin = d3.min(data, (d)-> d.yMin)
+    yBandMax = d3.max(data, (d)-> d.yMax)
+    y2BandMin = d3.min(data, (d)-> d.y2Min)
+    y2BandMax = d3.max(data, (d)-> d.y2Max)
+
+    ymin_a = [
+      y1min
+      y2min
+      yBandMin
+      y2BandMin
+    ]
+
+    ymax_a = [
+      y1max
+      y2max
+      yBandMax
+      y2BandMax
+    ]
+
+    ymin = d3.min(ymin_a)
+    ymax = d3.max(ymax_a)
+
+    # ymin = if y2min < y1min then y2min else y1min
+    # ymax = if y2max > y1max then y2max else y1max
 
     # Restore Viewability if Y-Min = Y-Max
     ymin = if ymin == ymax then ymin * 0.8 else ymin
@@ -184,7 +270,6 @@ window.Plotting.LinePlot = class LinePlot
       .attr("width", @definition.dimensions.width)
       .attr("height", @definition.dimensions.height)
 
-
     # Append a Clip Path
     @svg.append("defs")
       .append("clipPath")
@@ -201,10 +286,10 @@ window.Plotting.LinePlot = class LinePlot
     # Append the X-Axis
     @svg.append("g")
       .attr("class", "line-plot-axis-x")
-      .attr("transform", "translate(0, #{bottomPadding})")
       .style("fill", "none")
       .style("stroke", @options.axisColor)
       .call(@definition.xAxis)
+      .attr("transform", "translate(0, #{bottomPadding})")
 
     # Add Text Labels to X-Axis (Only if Large Scale Theme)
     if @options.theme isnt 'minimum'
@@ -216,15 +301,30 @@ window.Plotting.LinePlot = class LinePlot
     # Append the Y-Axis
     @svg.append("g")
       .attr("class", "line-plot-axis-y")
-      .attr("transform", "translate(#{leftPadding}, 0)")
       .style("fill", "none")
       .style("stroke", @options.axisColor)
       .style("font-size", @options.font.size)
       .style("font-weight", @options.font.weight)
       .call(@definition.yAxis)
+      .attr("transform", "translate(#{leftPadding}, 0)")
 
+    @lineband = @svg.append("path")
+      .datum(@data)
+      .attr("d", @definition.area)
+      .attr("class", "line-plot-area")
+      .style("fill", "rgb(52, 152, 219)")
+      .style("opacity", 0.15)
+      .style("stroke", "BLACK")
 
-    # Append the Line Path
+    @lineband2 = @svg.append("path")
+      .datum(@data)
+      .attr("d", @definition.area2)
+      .attr("class", "line-plot-area")
+      .style("fill", "rgb(46, 204, 113)")
+      .style("opacity", 0.15)
+      .style("stroke", "BLACK")
+
+    # Append the Line Path 1
     @svg.append("g")
       .attr("clip-path", "url(\##{@options.target}_clip)")
       .append("path")
@@ -235,13 +335,14 @@ window.Plotting.LinePlot = class LinePlot
       .style("stroke-width",
           Math.round(Math.pow(@definition.dimensions.width, 0.1)))
       .style("fill", "none")
-##########################yoloswag
+
+    # Append the Line Path 2
     @svg.append("g")
       .attr("clip-path", "url(\##{@options.target}_clip)")
       .append("path")
       .datum(@data)
-      .attr("d", @definition.line)
-      .attr("class", "line-plot-path")
+      .attr("d", @definition.line2)
+      .attr("class", "line-plot-path2")
       .style("stroke", @options.line2Color)
       .style("stroke-width",
           Math.round(Math.pow(@definition.dimensions.width, 0.1)))
@@ -250,26 +351,38 @@ window.Plotting.LinePlot = class LinePlot
     # Create Crosshairs
     @crosshairs = @svg.append("g")
       .attr("class", "crosshair")
-    # Create Horizontal line
+
+    # Create Vertical line
     @crosshairs.append("line")
       .attr("class", "crosshair-x")
       .style("stroke", @options.crosshairX.color)
       .style("stroke-width", @options.crosshairX.weight)
       .style("fill", "none")
-    # Create Vertical Line
-    @crosshairs.append("line")
-      .attr("class", "crosshair-y")
-      .style("stroke", @options.crosshairY.color)
-      .style("stroke-width", @options.crosshairY.weight)
-      .style("fill", "none")
 
     _ = @
 
-    # Create Focus Circle
+    # Create Focus Circles and Labels
     @focusCircle = @svg.append("circle")
-      .attr("r", 5)
+      .attr("r", 4)
       .attr("class", "focusCircle")
       .attr("fill", @options.focusCircle.color)
+
+    @focusText = @svg.append("text")
+      .attr("class", "focusText")
+      .attr("x", 9)
+      .attr("y", 7)
+      .style("stroke", @options.focusCircle.color)
+
+    @focusCircle2 = @svg.append("circle")
+      .attr("r", 4)
+      .attr("class", "focusCircle2")
+      .attr("fill", @options.focusCircle2.color)
+
+    @focusText2 = @svg.append("text")
+      .attr("class", "focusText2")
+      .attr("x", 9)
+      .attr("y", 7)
+      .style("stroke", @options.focusCircle2.color)
 
     # Move Crosshairs and Focus Circle Based on Mouse Location
     @svg.append("rect")
@@ -277,26 +390,37 @@ window.Plotting.LinePlot = class LinePlot
       .attr("class", "overlay")
       .attr("width", innerWidth)
       .attr("height", innerHeight)
-      #.attr("transform", "translate(#{leftPadding}, #{topPadding})")
+      .attr("transform", "translate(#{leftPadding}, #{topPadding})")
       .style("fill", "none")
       .style("pointer-events", "all")
       .on("mouseover", () ->
         _.crosshairs.style("display", null)
+        _.focusCircle.style("display", null)
+        _.focusCircle2.style("display", null)
+        _.focusText.style("display", null)
+        _.focusText2.style("display", null)
       )
       .on("mouseout", () ->
         _.crosshairs.style("display", "none")
+        _.focusCircle.style("display", "none")
+        _.focusCircle2.style("display", "none")
+        _.focusText.style("display", "none")
+        _.focusText2.style("display", "none")
       )
 
       .on("mousemove", (d) ->
         mouse = d3.mouse @
-        x0 = _.definition.x.invert(mouse[0])
+        x0 = _.definition.x.invert(mouse[0] + leftPadding)
         i = _.bisectDate(d, x0, 1)
-        d0 = d[i]
-        d1 = d[i + 1]
-        d = if x0 - (d0.Date) > (d1.Date) - x0 then d1 else d0
-        dy = _.definition.y(d.y)
+        min = x0.getMinutes()
+        d0 = d[i - 1]
+        d1 = d[i]
+        d = d0
+        d = if min >= 30 then d1 else d0
         dx = _.definition.x(d.x)
-        
+        dy = _.definition.y(d.y)
+        dy2 = _.definition.y(d.y2)
+
         _.crosshairs.select(".crosshair-x")
           .attr("x1", mouse[0])
           .attr("y1", topPadding)
@@ -307,8 +431,22 @@ window.Plotting.LinePlot = class LinePlot
         _.focusCircle
           .attr("cx", dx)
           .attr("cy", dy)
-          #.attr("transform", "translate(#{leftPadding}, 0)")
+
+        _.focusText
+          .attr("x", dx + leftPadding / 10)
+          .attr("y", dy - topPadding / 10)
+          .text(d.y.toFixed(1) + " " + "°F")
+
+        _.focusCircle2
+          .attr("cx", dx)
+          .attr("cy", dy2)
+
+        _.focusText2
+          .attr("x", dx + leftPadding / 10)
+          .attr("y", dy2 - topPadding / 10)
+          .text(d.y2.toFixed(1) + " " + "°F")
         )
+
 
   update: (data) ->
     preError = "#{@preError}update()"
@@ -317,15 +455,22 @@ window.Plotting.LinePlot = class LinePlot
     # Append New Data
     dtOffset = new Date @definition.x.max
     for key, row of data
-      @data.push(
+      dtaRow =
         x: @parseDate(row[@options.x.variable])
         y: row[@options.y.variable]
-      )
+      if @options.y2.variable != null
+        dtaRow.y2 = row[@options.y2.variable]
+
+      @data.push(dtaRow)
 
     # Pre-Append Data For Smooth transform
     @svg.select(".line-plot-path")
       .datum(@data)
       .attr("d", @definition.line)
+
+    @svg.select(".line-plot-path2")
+      .datum(@data)
+      .attr("d", @definition.line2)
 
     @calculateAxisDims @data
     dtDiff = @definition.x.max - dtOffset
@@ -359,3 +504,9 @@ window.Plotting.LinePlot = class LinePlot
       .transition()
       .duration(@options.transitionDuration)
       .attr("d", @definition.line)
+
+    @svg.select(".line-plot-path2")
+      .datum(@data)
+      .transition()
+      .duration(@options.transitionDuration)
+      .attr("d", @definition.line2)
--- js/plotting.js
+++ js/plotting.js
@@ -293,9 +293,23 @@
           min: null,
           max: null
         },
+        yBand: {
+          minVariable: null,
+          maxVariable: null
+        },
+        y2: {
+          variable: null,
+          ticks: 5,
+          min: null,
+          max: null
+        },
+        y2Band: {
+          minVariable: null,
+          maxVariable: null
+        },
         transitionDuration: 300,
-        line1Color: "rgb(41,128,185)",
-        line2Color: "rgb(211, 84, 0)",
+        line1Color: "rgb(41, 128, 185)",
+        line2Color: "rgb(39, 174, 96)",
         weight: 2,
         axisColor: "rgb(0,0,0)",
         font: {
@@ -307,7 +321,10 @@
           color: "rgb(149, 165, 166)"
         },
         focusCircle: {
-          color: "rgb(44, 62, 80)"
+          color: "rgb(41, 128, 185)"
+        },
+        focusCircle2: {
+          color: "rgb(39, 174, 96)"
         }
       };
       if (options.x) {
@@ -315,6 +332,9 @@
       }
       if (options.y) {
         options.y = Object.mergeDefaults(options.y, defaults.y);
+      }
+      if (options.y2) {
+        options.y2 = Object.mergeDefaults(options.y2, defaults.y2);
       }
       this.options = Object.mergeDefaults(options, defaults);
       this.log = function() {
@@ -340,6 +360,17 @@
           x: this.parseDate(row[this.options.x.variable]),
           y: row[this.options.y.variable]
         };
+        if (this.options.y2.variable !== null) {
+          this.data[key].y2 = row[this.options.y2.variable];
+        }
+        if (this.options.yBand.minVariable !== null && this.options.yBand.maxVariable !== null) {
+          this.data[key].yMin = row[this.options.yBand.minVariable];
+          this.data[key].yMax = row[this.options.yBand.maxVariable];
+        }
+        if (this.options.y2Band.minVariable !== null && this.options.y2Band.maxVariable !== null) {
+          this.data[key].y2Min = row[this.options.y2Band.minVariable];
+          this.data[key].y2Max = row[this.options.y2Band.maxVariable];
+        }
       }
       this.getDefinition();
       if (window.attachEvent) {
@@ -368,12 +399,37 @@
       this.calculateAxisDims(this.data);
       this.definition.xAxis = d3.axisBottom().scale(this.definition.x).ticks(Math.round($(this.options.target).width() / 100));
       this.definition.yAxis = d3.axisLeft().scale(this.definition.y).ticks(this.options.y.ticks);
-      return this.definition.line = d3.line().defined(function(d) {
+      this.definition.line = d3.line().defined(function(d) {
         return !isNaN(d.y) && d.y !== null;
       }).x(function(d) {
         return _.definition.x(d.x);
       }).y(function(d) {
         return _.definition.y(d.y);
+      }).curve(d3.curveCatmullRom.alpha(0.5));
+      this.definition.line2 = d3.line().defined(function(d) {
+        return !isNaN(d.y2) && d.y2 !== null;
+      }).x(function(d) {
+        return _.definition.x(d.x);
+      }).y(function(d) {
+        return _.definition.y(d.y2);
+      }).curve(d3.curveCatmullRom.alpha(0.5));
+      this.definition.area = d3.area().defined(function(d) {
+        return !isNaN(d.y) && d.y !== null;
+      }).x(function(d) {
+        return _.definition.x(d.x);
+      }).y0(function(d) {
+        return _.definition.y(d.yMin);
+      }).y1(function(d) {
+        return _.definition.y(d.yMax);
+      }).curve(d3.curveCatmullRom.alpha(0.5));
+      return this.definition.area2 = d3.area().defined(function(d) {
+        return !isNaN(d.y) && d.y !== null;
+      }).x(function(d) {
+        return _.definition.x(d.x);
+      }).y0(function(d) {
+        return _.definition.y(d.y2Min);
+      }).y1(function(d) {
+        return _.definition.y(d.y2Max);
       }).curve(d3.curveCatmullRom.alpha(0.5));
     };
 
@@ -407,7 +463,7 @@
     };
 
     LinePlot.prototype.calculateAxisDims = function(data) {
-      var preError, xmax, xmin, ymax, ymin;
+      var preError, xmax, xmin, y1max, y1min, y2BandMax, y2BandMin, y2max, y2min, yBandMax, yBandMin, ymax, ymax_a, ymin, ymin_a;
       preError = this.preError + "calculateAxisDims(data)";
       xmin = this.options.x.min === null ? d3.min(data, function(d) {
         return d.x;
@@ -415,12 +471,34 @@
       xmax = this.options.x.max === null ? d3.max(data, function(d) {
         return d.x;
       }) : this.parseDate(this.options.x.max);
-      ymin = this.options.y.min === null ? d3.min(data, function(d) {
+      y1min = this.options.y.min === null ? d3.min(data, function(d) {
         return d.y;
       }) : this.options.y.min;
-      ymax = this.options.y.max === null ? d3.max(data, function(d) {
+      y1max = this.options.y.max === null ? d3.max(data, function(d) {
         return d.y;
       }) : this.options.y.max;
+      y2min = this.options.y2.min === null ? d3.min(data, function(d) {
+        return d.y2;
+      }) : this.options.y2.min;
+      y2max = this.options.y2.max === null ? d3.max(data, function(d) {
+        return d.y2;
+      }) : this.options.y2.max;
+      yBandMin = d3.min(data, function(d) {
+        return d.yMin;
+      });
+      yBandMax = d3.max(data, function(d) {
+        return d.yMax;
+      });
+      y2BandMin = d3.min(data, function(d) {
+        return d.y2Min;
+      });
+      y2BandMax = d3.max(data, function(d) {
+        return d.y2Max;
+      });
+      ymin_a = [y1min, y2min, yBandMin, y2BandMin];
+      ymax_a = [y1max, y2max, yBandMax, y2BandMax];
+      ymin = d3.min(ymin_a);
+      ymax = d3.max(ymax_a);
       ymin = ymin === ymax ? ymin * 0.8 : ymin;
       ymax = ymin === ymax ? ymax * 1.2 : ymax;
       this.definition.x.min = xmin;
@@ -450,50 +528,73 @@
       this.svg.append("defs").append("clipPath").attr("id", this.options.target + "_clip").append("rect").attr("width", innerWidth).attr("height", innerHeight).attr("transform", "translate(" + leftPadding + ", " + topPadding + ")");
       this.definition.x.domain([this.definition.x.min, this.definition.x.max]);
       this.definition.y.domain([this.definition.y.min, this.definition.y.max]).nice();
-      this.svg.append("g").attr("class", "line-plot-axis-x").attr("transform", "translate(0, " + bottomPadding + ")").style("fill", "none").style("stroke", this.options.axisColor).call(this.definition.xAxis);
+      this.svg.append("g").attr("class", "line-plot-axis-x").style("fill", "none").style("stroke", this.options.axisColor).call(this.definition.xAxis).attr("transform", "translate(0, " + bottomPadding + ")");
       if (this.options.theme !== 'minimum') {
         this.svg.select(".line-plot-axis-x").selectAll("text").style("font-size", this.options.font.size).style("font-weight", this.options.font.weight);
       }
-      this.svg.append("g").attr("class", "line-plot-axis-y").attr("transform", "translate(" + leftPadding + ", 0)").style("fill", "none").style("stroke", this.options.axisColor).style("font-size", this.options.font.size).style("font-weight", this.options.font.weight).call(this.definition.yAxis);
+      this.svg.append("g").attr("class", "line-plot-axis-y").style("fill", "none").style("stroke", this.options.axisColor).style("font-size", this.options.font.size).style("font-weight", this.options.font.weight).call(this.definition.yAxis).attr("transform", "translate(" + leftPadding + ", 0)");
+      this.lineband = this.svg.append("path").datum(this.data).attr("d", this.definition.area).attr("class", "line-plot-area").style("fill", "rgb(52, 152, 219)").style("opacity", 0.15).style("stroke", "BLACK");
+      this.lineband2 = this.svg.append("path").datum(this.data).attr("d", this.definition.area2).attr("class", "line-plot-area").style("fill", "rgb(46, 204, 113)").style("opacity", 0.15).style("stroke", "BLACK");
       this.svg.append("g").attr("clip-path", "url(\#" + this.options.target + "_clip)").append("path").datum(this.data).attr("d", this.definition.line).attr("class", "line-plot-path").style("stroke", this.options.line1Color).style("stroke-width", Math.round(Math.pow(this.definition.dimensions.width, 0.1))).style("fill", "none");
-      this.svg.append("g").attr("clip-path", "url(\#" + this.options.target + "_clip)").append("path").datum(this.data).attr("d", this.definition.line).attr("class", "line-plot-path").style("stroke", this.options.line2Color).style("stroke-width", Math.round(Math.pow(this.definition.dimensions.width, 0.1))).style("fill", "none");
+      this.svg.append("g").attr("clip-path", "url(\#" + this.options.target + "_clip)").append("path").datum(this.data).attr("d", this.definition.line2).attr("class", "line-plot-path2").style("stroke", this.options.line2Color).style("stroke-width", Math.round(Math.pow(this.definition.dimensions.width, 0.1))).style("fill", "none");
       this.crosshairs = this.svg.append("g").attr("class", "crosshair");
       this.crosshairs.append("line").attr("class", "crosshair-x").style("stroke", this.options.crosshairX.color).style("stroke-width", this.options.crosshairX.weight).style("fill", "none");
-      this.crosshairs.append("line").attr("class", "crosshair-y").style("stroke", this.options.crosshairY.color).style("stroke-width", this.options.crosshairY.weight).style("fill", "none");
       _ = this;
-      this.focusCircle = this.svg.append("circle").attr("r", 5).attr("class", "focusCircle").attr("fill", this.options.focusCircle.color);
-      return this.svg.append("rect").datum(this.data).attr("class", "overlay").attr("width", innerWidth).attr("height", innerHeight).style("fill", "none").style("pointer-events", "all").on("mouseover", function() {
-        return _.crosshairs.style("display", null);
+      this.focusCircle = this.svg.append("circle").attr("r", 4).attr("class", "focusCircle").attr("fill", this.options.focusCircle.color);
+      this.focusText = this.svg.append("text").attr("class", "focusText").attr("x", 9).attr("y", 7).style("stroke", this.options.focusCircle.color);
+      this.focusCircle2 = this.svg.append("circle").attr("r", 4).attr("class", "focusCircle2").attr("fill", this.options.focusCircle2.color);
+      this.focusText2 = this.svg.append("text").attr("class", "focusText2").attr("x", 9).attr("y", 7).style("stroke", this.options.focusCircle2.color);
+      return this.svg.append("rect").datum(this.data).attr("class", "overlay").attr("width", innerWidth).attr("height", innerHeight).attr("transform", "translate(" + leftPadding + ", " + topPadding + ")").style("fill", "none").style("pointer-events", "all").on("mouseover", function() {
+        _.crosshairs.style("display", null);
+        _.focusCircle.style("display", null);
+        _.focusCircle2.style("display", null);
+        _.focusText.style("display", null);
+        return _.focusText2.style("display", null);
       }).on("mouseout", function() {
-        return _.crosshairs.style("display", "none");
+        _.crosshairs.style("display", "none");
+        _.focusCircle.style("display", "none");
+        _.focusCircle2.style("display", "none");
+        _.focusText.style("display", "none");
+        return _.focusText2.style("display", "none");
       }).on("mousemove", function(d) {
-        var d0, d1, dx, dy, i, mouse, x0;
+        var d0, d1, dx, dy, dy2, i, min, mouse, x0;
         mouse = d3.mouse(this);
-        x0 = _.definition.x.invert(mouse[0]);
+        x0 = _.definition.x.invert(mouse[0] + leftPadding);
         i = _.bisectDate(d, x0, 1);
-        d0 = d[i];
-        d1 = d[i + 1];
-        d = x0 - d0.Date > d1.Date - x0 ? d1 : d0;
-        dy = _.definition.y(d.y);
+        min = x0.getMinutes();
+        d0 = d[i - 1];
+        d1 = d[i];
+        d = d0;
+        d = min >= 30 ? d1 : d0;
         dx = _.definition.x(d.x);
+        dy = _.definition.y(d.y);
+        dy2 = _.definition.y(d.y2);
         _.crosshairs.select(".crosshair-x").attr("x1", mouse[0]).attr("y1", topPadding).attr("x2", mouse[0]).attr("y2", innerHeight + topPadding).attr("transform", "translate(" + leftPadding + ", 0)");
-        return _.focusCircle.attr("cx", dx).attr("cy", dy);
+        _.focusCircle.attr("cx", dx).attr("cy", dy);
+        _.focusText.attr("x", dx + leftPadding / 10).attr("y", dy - topPadding / 10).text(d.y.toFixed(1) + " " + "°F");
+        _.focusCircle2.attr("cx", dx).attr("cy", dy2);
+        return _.focusText2.attr("x", dx + leftPadding / 10).attr("y", dy2 - topPadding / 10).text(d.y2.toFixed(1) + " " + "°F");
       });
     };
 
     LinePlot.prototype.update = function(data) {
-      var _, dtDiff, dtOffset, key, preError, row;
+      var _, dtDiff, dtOffset, dtaRow, key, preError, row;
       preError = this.preError + "update()";
       _ = this;
       dtOffset = new Date(this.definition.x.max);
       for (key in data) {
         row = data[key];
-        this.data.push({
+        dtaRow = {
           x: this.parseDate(row[this.options.x.variable]),
           y: row[this.options.y.variable]
-        });
+        };
+        if (this.options.y2.variable !== null) {
+          dtaRow.y2 = row[this.options.y2.variable];
+        }
+        this.data.push(dtaRow);
       }
       this.svg.select(".line-plot-path").datum(this.data).attr("d", this.definition.line);
+      this.svg.select(".line-plot-path2").datum(this.data).attr("d", this.definition.line2);
       this.calculateAxisDims(this.data);
       dtDiff = this.definition.x.max - dtOffset;
       this.log(preError + " Date Diff Calcs (dtOffset, @def.x.min, dtDiff)", dtOffset, this.definition.x.max, this.dtDiff);
@@ -501,7 +602,8 @@
       this.definition.y.domain([this.definition.y.min, this.definition.y.max]).nice();
       this.svg.select(".line-plot-axis-x").style("font-size", this.options.font.size).style("font-weight", this.options.font.weight).transition().duration(this.options.transitionDuration).ease(d3.easeLinear).call(this.definition.xAxis);
       this.svg.select(".line-plot-axis-y").style("font-size", this.options.font.size).style("font-weight", this.options.font.weight).transition().duration(this.options.transitionDuration).ease(d3.easeLinear).call(this.definition.yAxis);
-      return this.svg.select(".line-plot-path").datum(this.data).transition().duration(this.options.transitionDuration).attr("d", this.definition.line);
+      this.svg.select(".line-plot-path").datum(this.data).transition().duration(this.options.transitionDuration).attr("d", this.definition.line);
+      return this.svg.select(".line-plot-path2").datum(this.data).transition().duration(this.options.transitionDuration).attr("d", this.definition.line2);
     };
 
     return LinePlot;
